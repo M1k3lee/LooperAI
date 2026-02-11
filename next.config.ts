e6c5493919225937541c2abdfb69af1d8c5d2d4e
M1k3lee <mikewady@gmail.com>
2026-02-11 12:46:17 +0000

    Switch config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export',
-  basePath: '/LooperAI',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
